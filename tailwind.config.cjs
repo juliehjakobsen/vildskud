2dee7b7450fecfb85a66b07777ccaf676c1421a4
Julie Jakobsen <juliehage@live.dk>
2022-10-10 12:02:02 +0200

    ny h2 størrelse

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
     },
     fontSize: {
       h1_mobil: "3.125rem",
-      h2_mobil: "1.563rem",
+      h2_mobil: "2rem",
       h3_mobil: "1.25rem",
       brodtekst_mobil: "1rem",
       h1_web: "6.25rem",
